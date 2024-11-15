e163b901f42fd52cf4e7cfa62bd3ecf400ef7052
Bryan Vera Lenis <bvera.est@gdeon.org>
2024-11-15 15:37:20 -0500

    Version 0.0 del proyecto, se crea el esquema

---
+++ src/main/java/com/ug/bookingplaneseats/BookingPlaneSeats.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ug.bookingplaneseats;
+
+import com.ug.bookingplaneseats.gui.Register;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author aleya
+ */
+public class BookingPlaneSeats {
+
+    public static void main(String[] args) {
+        //System.out.println("Hello World!");
+        Register registerPanel = new Register();
+        registerPanel.setVisible(true);
+    }
+}
