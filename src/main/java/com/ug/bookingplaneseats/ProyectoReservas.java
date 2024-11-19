d55b3304b7f8cf8d960f5b685c23d939b6d9d5e3
Bryan Vera Lenis <bvera.est@gdeon.org>
2024-11-18 23:21:15 -0500

    Modificacion del proyecto

---
+++ src/main/java/com/ug/bookingplaneseats/ProyectoReservas.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ug.bookingplaneseats;
+
+/**
+ *
+ * @author Misha
+ */
+public class ProyectoReservas {
+
+    public static void main(String[] args) {
+        Registro registro = new Registro();
+        registro.setVisible(true);
+    }
+}
